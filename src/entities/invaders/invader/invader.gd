d0c011a5dd911e4a8b0fb7cccd768ff19e4d4a23
Robert-Beckett <robert.beckett@onion.red>
2025-08-23 17:19:57 -0300

    - Completed basic manager implementation - Cleaned up movement logic on Invaders to be more reliable and   generally simpler

--- src/entities/invaders/invader/invader.gd
+++ src/entities/invaders/invader/invader.gd
@@ -36,11 +36,11 @@ var current_frame_time: float = 0.0
 var sprite: Sprite2D = null
 var visual_width: float = 0.0
 
-@export var strafe_box: Rect2 = Rect2(self.position, Vector2(0, 0))
+## A rectangle used to visually define the bounding box for the
+##  invaders vertical movement.
+@export var strafe_box: Rect2 = Rect2(Vector2(0, 0), Vector2(0, 60))
 var strafe_left_x: float = 0.0
 var strafe_right_x: float = 0.0
-
-var is_dropping: bool = false
 
 func _ready() -> void:
 	strafe_left_x =  self.position.x
@@ -70,25 +70,17 @@ func tick() -> void:
 	self.handle_movement()
 
 func handle_movement() -> void:
-	if is_dropping:
-		self.position.y += self.drop_speed
-		self.is_dropping = false
-		return
-
 	var move_vector = Vector2(strafe_speed, 0)
 	if current_direction == Direction.LEFT:
 		move_vector = -move_vector
 	
-	self.position += move_vector
+	var new_position = self.position + move_vector
 
-	if self.position.x >= strafe_right_x - self.visual_width:
+	if new_position.x > strafe_right_x - self.visual_width:
+		self.position.y += self.drop_speed
 		self.current_direction = Direction.LEFT
-		self.is_dropping = true
-	elif self.position.x <= strafe_left_x:
+	elif new_position.x < strafe_left_x:
+		self.position.y += self.drop_speed
 		self.current_direction = Direction.RIGHT
-		self.is_dropping = true
-	
-	self.clamp_to_strafe_box()
-
-func clamp_to_strafe_box() -> void:
-	self.position.x = clamp(self.position.x, strafe_left_x, strafe_right_x - visual_width)
+	else:
+		self.position = new_position
--- src/entities/invaders/invaders_manager/invaders_manager.gd
+++ src/entities/invaders/invaders_manager/invaders_manager.gd
@@ -1,3 +1,61 @@
+class_name InvaderManager
 extends Node2D
 
+## The number of columns to fill with invaders
+@export var columns: int = 1
+## The number of rows to fill with invaders
+@export var rows: int = 1
 
+## The space between each invader in a row
+@export var row_gap: float = 60
+## The space between each invader in a column
+@export var column_gap: float = 60
+
+## The width of an invader
+@export var invader_width: float = 60
+## The height of an invader
+@export var invader_height: float = 60
+
+## The width that the invader is allowed to strafe within
+@export var invader_strafe_width: float = 180
+
+## The length of a frame in seconds
+@export var frame_length: float = 0.5
+
+var current_frame_time: float = 0.0
+
+## The Invader scene that the manager should load
+##  to generate Invader instances from
+@export var invader_scene: PackedScene
+
+var invaders: Array[Invader] = []
+
+
+func _ready() -> void:
+	self.create_invaders()
+
+func _physics_process(delta: float) -> void:
+	self.current_frame_time += delta
+	if self.current_frame_time >= self.frame_length:
+		self.current_frame_time -= self.frame_length
+		self.tick()
+
+func create_invaders() -> void:
+	var current_position: Vector2 = self.position
+	for i in range(0, rows):
+		for j in range(0, columns):
+			var new_invader: Invader = invader_scene.instantiate()
+			new_invader.position = current_position
+			new_invader.strafe_box = Rect2(Vector2(0, 0), 
+										   Vector2(invader_strafe_width, 0))
+
+			add_child(new_invader)
+			self.invaders.push_back(new_invader)
+
+			current_position.x += invader_width + column_gap
+		current_position.x = self.position.x
+		current_position.y += invader_height + row_gap
+
+func tick() -> void:
+	for invader in self.invaders:
+		invader.tick()
